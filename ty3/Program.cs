ddd548d53007b9ffb63db63075cc2ac8b3a473ef
kh-6 <khawla6xavi@gmail.com>
2017-08-10 19:05:50 +0300

    Add project files.

---
+++ ty3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ty3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("roj");
+            Console.ReadKey();
+        }
+    }
+}
